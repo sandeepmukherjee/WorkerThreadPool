2b6fc689b7b400532624b3dab49df7fc984cb4c2
Sandeep Mukherjee <mukherjee.sandeep@gmail.com>
2014-06-29 19:29:42 -0700

    Completed Internall Error class.

--- WTPExceptions.cpp
+++ WTPExceptions.cpp
@@ -5,6 +5,6 @@
 WTP::InternalError::InternalError(const std::string& message, const char *file, int line)
 {
     std::ostringstream buf;
-    buf << std::string(file) << ":" << line;
+    buf << "WTP INTERR:" << std::string(file) << ":" << line << ":" << message;
     msg = buf.str();
 }
